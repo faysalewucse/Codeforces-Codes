a56c183b7bee3f68092e7f8ec9a189ce277ffdc7
faysalewucse <faysal.ewucse@gmail.com>
2022-07-03 23:22:01 +0600

    100A

--- 110A.cpp
+++ 110A.cpp
@@ -1,5 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    long long int n;
+    int flag =0;
+    cin>>n;
+    while(n > 0)
+    {
+        int d = n%10;
+        n /= 10;
+        if(d == 4 || d == 7)
+        {
+            flag++;
+        }
+    }
+
+    if(flag == 4 || flag == 7) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
 
 }
